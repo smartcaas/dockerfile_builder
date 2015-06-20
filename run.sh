8e4c4296012f0858825cd819f406b00eff6a5d00
leo108 <leo108@qq.com>
2015-06-20 20:39:35 +0800

    sed

--- run.sh
+++ run.sh
@@ -21,9 +21,9 @@ sed -i "s/{DB_HOST}/${MYSQL_PORT_3306_TCP_ADDR}/g" $confFile \
 && sed -i "s/{DB_USER}/${DB_USER}/g" $confFile \
 && sed -i "s/{DB_NAME}/${DB_NAME}/g" $confFile \
 && sed -i "s/{DB_PWD}/${DB_PWD}/g" $confFile \
-&& sed -i "s/{UIC_URL}/${UIC_URL}/g" $confFile \
+&& sed -i "s#{UIC_URL}#${UIC_URL}#g" $confFile \
 && sed -i "s/{UIC_TOKEN}/${UIC_TOKEN}/g" $confFile \
-&& sed -i "s/{REGISTRY_URL}/${REGISTRY_URL}/g" $confFile
+&& sed -i "s#{REGISTRY_URL}#${REGISTRY_URL}#g" $confFile
 
-cd $GOPATH/src/github.com/dinp
+cd $GOPATH/src/github.com/dinp/builder/
 ./builder
